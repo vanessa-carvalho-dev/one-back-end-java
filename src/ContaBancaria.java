b1fe5354eeedf1cdd6ec6306eae8886f1640af71
Vanessa Carvalho <devanessa.carvalho@gmail.com>
2024-05-03 17:05:33 -0300

    feat: add menu de opções e consultar saldo

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -29,7 +29,33 @@ public class ContaBancaria {
         System.out.println("Digite a opção desejada:");
         int opcao = leitorTeclado.nextInt();
 
+        while(opcao != 4){
+            switch (opcao){
+                case 1:
+                    System.out.println(String.format("O saldo atual é R$ %.2f", saldo));
+                    break;
+                case 2:
+                    System.out.println("Em desenvolvimento...");
+                    break;
+                case 3:
+                    System.out.println("Em desenvolvimento...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
 
+            System.out.println("""
+                Operações
+
+                1 - Consultar saldo
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """);
+
+            System.out.println("Digite a opção desejada:");
+            opcao = leitorTeclado.nextInt();
+        }
 
     }
 }
